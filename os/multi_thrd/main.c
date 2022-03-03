ab9f3bcb2d4abac16dd424f04b122a1fffe2da06
Luo-freewell-man <1063376141@qq.com>
2022-03-03 19:34:56 +0800

    thread

--- os/multi_thrd/main.c
+++ os/multi_thrd/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
-#include <thread.h>
+#include "thread.h"
+
+int *globalptr;
+
+void thread1(void){
+	int num = 3000;
+	globalptr = &num;
+}
+   
+void thread2(void){
+	printf("thread1's number is %d\r\n",*globalptr);
+}
 
 int main(void){
-
+	create(thread1);
+	create(thread2);
+	return 0;
 }
