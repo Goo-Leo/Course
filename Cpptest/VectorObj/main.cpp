5d5353583699cd383894d1a2654b6ab266524669
Luo-freewell-man <1063376141@qq.com>
2022-03-03 20:05:04 +0800

    vector read & go

--- Cpptest/VectorObj/main.cpp
+++ Cpptest/VectorObj/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
+const int seq_size = 10;
+
 int main(void){
 	vector<int> seq(seq_size);
+	int sum;
+	cout << "init your seqence1:" << endl;
+	for (int i = 0; i < seq_size; i++) {
+		cin >>seq[i];	
+	}
+	for (int i = 0; i < seq_size; i++) {
+		sum += seq[i];
+	}
+	cout << "seq's summary is: " << sum
+		<< "  and average is: " << sum/seq_size << endl;
+
+	return 0;
 }
